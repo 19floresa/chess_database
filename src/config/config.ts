c2db54bf0c974732d8ba042897421615f95d06c5
19floresa <alexander.flores0920@gmail.com>
2026-01-03 23:18:16 -0800

    created config for database

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,11 +5,19 @@ dotenv.config()
 interface Config {
     port: number;
     nodeEnv: string;
+    dbUsername: string,
+    dbPassword: string,
+    dbName: string
+    dbPort: number,
 }
 
 const config: Config = {
     port: Number(process.env.PORT) || 3079,
     nodeEnv: process.env.NODE_ENV || "development",
+    dbUsername: process.env.DATABASE_USERNAME || "",
+    dbPassword: process.env.DATABASE_PASSWORD || "",
+    dbName: process.env.DATABASE_NAME || "",
+    dbPort: Number(process.env.DATABASE_PORT || 5432),
 }
 
 export default config
