94865fdb27f2b3ace0aecca69b6d8643345b47b8
19floresa <alexander.flores0920@gmail.com>
2026-01-04 23:15:11 -0800

    reworked database schema

--- src/database/db.ts
+++ src/database/db.ts
@@ -9,25 +9,25 @@ const sql = postgres({
     password: config.dbPassword,
 })
 
-await sql`CREATE TABLE IF NOT EXISTS players( id           INTEGER     GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-                                              username     VARCHAR(64) NOT NULL, 
-                                              password     VARCHAR(64) NOT NULL,
-                                              time_created TIMESTAMP   NOT NULL
+await sql`CREATE TABLE IF NOT EXISTS players( id            INTEGER      GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+                                              username      VARCHAR(64)  NOT NULL, 
+                                              password      VARCHAR(64)  NOT NULL,
+                                              time_created  TIMESTAMP    NOT NULL
                                             );`
 
-await sql`CREATE TABLE IF NOT EXISTS game_info( id         BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                                                game_steps BIT(3)[4][] NOT NULL
-                                               );`
-
-await sql`CREATE TABLE IF NOT EXISTS previous_games( game_info_id       BIGINT      REFERENCES game_info(id), 
-                                                     player_id          INTEGER     REFERENCES players(id),
-                                                     player_id_opponent INTEGER     REFERENCES players(id),
-                                                     start_time         TIMESTAMP   NOT NULL,
-                                                     end_time           TIMESTAMP   NOT NULL,
-                                                     is_player_light    BOOLEAN     NOT NULL,
-                                                     is_player_winner   BOOLEAN     NOT NULL,
-                                                     game_status        VARCHAR(64) NOT NULL,
+await sql`CREATE TABLE IF NOT EXISTS previous_games( game_info_id        BIGINT   REFERENCES game_info(id), 
+                                                     player_id           INTEGER  REFERENCES players(id),
                                                      PRIMARY KEY(game_info_id, player_id)
                                                     );`
+
+await sql`CREATE TABLE IF NOT EXISTS game_info( id                BIGINT       GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                                                player_id_light   INTEGER      REFERENCES players(id),
+                                                player_id_dark    INTEGER      REFERENCES players(id),
+                                                player_id_winner  INTEGER      REFERENCES players(id),
+                                                start_time        TIMESTAMP    NOT NULL,
+                                                end_time          TIMESTAMP    NOT NULL,
+                                                game_status       VARCHAR(64)  NOT NULL,
+                                                game_steps        BIT(3)[4][]  NOT NULL
+                                               );`
 
 export default sql
