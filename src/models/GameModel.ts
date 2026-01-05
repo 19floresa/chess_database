ff63f1ca71ee494cf03838b9762fd5e0529db2a2
19floresa <alexander.flores0920@gmail.com>
2026-01-04 23:16:14 -0800

    created game model to store data in database

---
+++ src/models/GameModel.ts
@@ -0,0 +1,125 @@
+import sql from "../database/db.ts"
+
+export interface previousGameEntry {
+    game_info_id: number
+    player_id: number
+}
+
+export interface gameInfoEntry {
+    player_id_light: number
+    player_id_dark: number
+    player_id_winner: number
+    start_time: string
+    end_time: string
+    game_status: string
+    game_steps: [ string, string, string, string ][]
+}
+
+export class GameModel
+{
+    readonly #infoColumns = [ "player_id_light", "player_id_dark", "player_id_winner", "start_time",
+                              "end_time",        "game_status",    "game_steps" ] as const
+    readonly #prevColumns = [ "game_info_id", "player_id"  ] as const
+
+    async createStoreEntry(idLight: number,
+                           idDark: number,
+                           idWinner: number,
+                           start: string,
+                           end: string,
+                           status: string,
+                           gameSteps: [ number, number, number, number ][]): Promise<void>
+    {
+        // Generate and store game information
+        const infoEntries: gameInfoEntry = this.createInfoEntry(idLight, 
+                                                                idDark, 
+                                                                idWinner, 
+                                                                start, 
+                                                                end, 
+                                                                status, 
+                                                                gameSteps)
+        const gameInfoId: number = await this.storeInfoEntry(infoEntries)
+        
+        // Generate and store Player game
+        const prevEntries: previousGameEntry[] = 
+        [
+            this.createPreviousGameEntry(gameInfoId, idLight), // player 1 entry
+            this.createPreviousGameEntry(gameInfoId, idDark)   // player 2 entry
+        ]
+        this.storePrevEntry(prevEntries)
+    }
+
+    createPreviousGameEntry(game_info_id: number,
+                            player_id: number): previousGameEntry
+    {
+        const entry: previousGameEntry =
+        {
+            game_info_id,
+            player_id,
+        }
+        return entry
+    }
+    
+    createInfoEntry(player_id_light: number,
+                    player_id_dark: number,
+                    player_id_winner: number,
+                    start_time: string,
+                    end_time: string,
+                    game_status: string,
+                    steps: [ number, number, number, number ][]): gameInfoEntry
+    {
+        const game_steps: [ string, string, string, string ][] = []
+        for (const step of steps)
+        {
+            const gameStep = this.transformStep(step)
+            game_steps.push(gameStep)
+        }
+        
+        const entry: gameInfoEntry =
+        {
+            player_id_light,
+            player_id_dark,
+            player_id_winner,
+            start_time,
+            end_time,
+            game_status,
+            game_steps,
+        }
+        return entry
+    }
+
+    async storeInfoEntry(entry: gameInfoEntry): Promise<number>
+    {
+        const columns = this.#infoColumns
+        const out = await sql`INSERT INTO game_info${sql(entry, columns)} returning id`
+        return out[0]!.id
+    }
+
+    async storePrevEntry(entries: previousGameEntry[]): Promise<void>
+    {
+        const columns = this.#prevColumns
+        const results = await sql.begin( async sql =>
+        {
+            let output = []
+            for (const entry of entries)
+            {
+                const out = await sql`INSERT INTO previous_games${sql(entry, columns)} returning game_info_id`
+                output.push(out)
+            }
+            return output
+        })
+
+        console.log(results)
+    }
+
+    numberToBinary = (num: number) =>  num.toString(2).padStart(3, "0")
+    transformStep(step: [ number, number, number, number ]): [ string, string, string, string ]
+    {
+        const [ x, y, x2, y2 ] = step
+        return  [ 
+            this.numberToBinary(x),
+            this.numberToBinary(y),
+            this.numberToBinary(x2),
+            this.numberToBinary(y2),
+        ]
+    }
+}
