013a92ab38a0e4710b48f64865a4a7972abe3f93
19floresa <alexander.flores0920@gmail.com>
2026-02-22 18:20:22 -0800

    added accepected cors origins

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { errorHandler } from "./middleware/errorHandler.ts"
 const app = express()
 
 app.use(cors({
-    origin: '*',//['http://player:3025', 'http://game:3056'],
+    origin: ['http://localhost:3025', 'http://localhost:3056'],
     methods: ['GET', 'POST',/* 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'*/],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
 }))
